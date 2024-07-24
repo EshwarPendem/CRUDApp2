7ba2a6fac4c1b54fdbe60790c425b7cbc1c2e6d9
EshwarPendem <94189528+EshwarPendem@users.noreply.github.com>
2024-07-24 07:20:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controller.ServeHome).Methods("GET")
 	r.HandleFunc("/getallrecords", controller.GetAllRecords).Methods("GET")
-	r.HandleFunc("/addrecord", controller.AddARecord).Methods("POST")               //validation string value instead of int
-	r.HandleFunc("/deleterecord/{name}", controller.DeleteByName).Methods("DELETE") // No data available response
+	r.HandleFunc("/addrecord", controller.AddARecord).Methods("POST")               
+	r.HandleFunc("/deleterecord/{name}", controller.DeleteByName).Methods("DELETE") 
 	r.HandleFunc("/getrecord/{name}", controller.GetByName).Methods("GET")
 	r.HandleFunc("/updaterecord/{name}", controller.UpdateByName).Methods("PUT")
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+"3000", r))
 }
 
-//data access layers
-//connection pooling
